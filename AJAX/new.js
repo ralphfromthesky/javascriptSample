793b5bb0b37b0deee173f012a8e73f7015145087
ybkja26 <ybkja26@gmail.com>
2023-09-13 17:13:55 +0800

    new

--- AJAX/new.js
+++ AJAX/new.js
@@ -3,11 +3,10 @@ $(document).ready(function () {
         $('#try').hide()
     })
 
-    $('input').focus(function () {
-        $(this).css('background-color', 'red')
-        $('.now').css('background-color', 'yellow')
-
-    })
+    // $('input').focus(function () {
+    //     $(this).css('background-color', 'red')
+    //     $('.now').css('background-color', 'yellow')
+    // })
     $('h1').click(function () {
         $(this).css('color', 'red')
         $('.ralph').css('color', 'yellow')
@@ -41,25 +40,62 @@ $(document).ready(function () {
         })
     })
 
-    $('#moreAnimate').click(function() {
+    $('#moreAnimate').click(function () {
         var div = $('#moreAnim');
-        div.animate({height: '300px', opacity: '0.4'}, "slow");
-        div.animate({width: '300px', opacity: '0.8'}, "slow");
-        div.animate({height: '100px', opacity: '0.4'}, "slow");
-        div.animate({width: '100px', opacity: '0.8'}, "slow");
-        
+        div.animate({ height: '300px', opacity: '0.4' }, "slow");
+        div.animate({ width: '300px', opacity: '0.8' }, "slow");
+        div.animate({ height: '100px', opacity: '0.4' }, "slow");
+        div.animate({ width: '100px', opacity: '0.8' }, "slow");
+
     })
-    $('#callback').click(function() {
-        $(this).hide(2000, function() {
+    $('#callback').click(function () {
+        $(this).hide(2000, () => {
             alert('hide with callback function')
         })
     })
-    $('#nocallback').click(function() {
+    $('#nocallback').click(function () {
         $(this).hide(2000);
         alert('no call back function')
     })
-$('#newP').click(function() {
-    $(this).html('<h1>ralph santolorin</h1>')
-})
-    
+    $('#newP').click(function () {
+        $(this).html('<h1>ralph santolorin</h1>')
+    })
+
+    $('#forms').submit(function (e) {
+        e.preventDefault();
+        var formData = {
+            name: $('#email').val().trim(),
+            password: $('#password').val().trim()
+        }
+        $('#email, #password').css('background-color', '');
+
+        if (formData.name === '') {
+            alert('no empty');
+            $('#email').css('background-color', 'red').focus()
+            return;
+        }
+        if (formData.password === '') {
+            alert('no empty please');
+            $('#password').css('background-color', 'yellow').focus()
+            return;
+        }
+        console.log(formData.name + formData.password)
+    })
+    console.log("Button clicked");
+    $('#textbutton').click(() => {
+        var newText = $('#thisText').text()
+        $('#thisText2').text(newText)
+        console.log(newText);
+    })
+    // $('#tryFocus').click(() =>{
+    //     alert('nowFocus')
+    // })
+    const tryFocus = document.getElementById('tryFocus')
+    const onFocus = () => {
+        alert('nowFocus')
+    }
+    tryFocus.addEventListener('click', onFocus)
+
+
+
 })
